1027ac00ea3aca238c8d669f90423a0cc18b3131
anjalidofe <dofeaa19.comp@coep.ac.in>
2021-08-24 12:59:27 +0530

    final changes

---
+++ stack.h
@@ -0,0 +1,9 @@
+
+
+typedef struct stack{
+    int *a;
+    int top;
+    int len;
+}stack;
+
+
---
+++ stack1.c
@@ -0,0 +1,50 @@
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include "stack.h"
+
+
+
+void inits(stack *s,int length){
+    s->top = -1;
+    s->len = length;
+    s->a = (int*)malloc(sizeof(int)*length);
+}
+
+void push(stack *s,int data){
+    if (s->top == s->len - 1)
+        return ;
+    s->top++;
+    s->a[s->top] = data;
+    return ;
+}
+
+int pop(stack *s){
+    if (s->top == -1)
+        return ;
+    int p;
+    p = s->a[s->top];
+    (s->a[s->top]) = NULL;
+    s->top--;
+    return p;
+}
+
+int isFulls(stack s) {
+    if(s.top == s.len-1)
+        return 1;
+    else
+        return 0;
+}
+
+int isEmptys(stack s) {
+    if(s.top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+int peek(stack s){
+    return s.a[s.top];
+}
+
